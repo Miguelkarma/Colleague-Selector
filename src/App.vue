6115b8b12d9d23cb5a273bcd01f59c81edd3d925
Miguelkarma <santos.p.miguel46@gmail.com>
2025-07-31 16:50:12 +0800

    event handling

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mx-auto">
+    <div class="flex flex-col items-center justify-center gap-3">
+      <a :href="randomLink">welcome to my domain</a>
+      <img v-bind="desc" />
+
+      <Events />
+    </div>
+  </div>
+</template>
+<script setup>
+import Events from "@/components/EventHandles.vue";
+const randomLink =
+  "https://www.reddit.com/r/learnprogramming/comments/v8gtq0/selftaught_developers_how_much_did_you_know_when/";
+
+const desc = {
+  src: new URL("./assets/pic.jpg", import.meta.url).href,
+  alt: "Hooman",
+  width: 200,
+  height: 300,
+};
+</script>
---
+++ src/components/EventHandles.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>This is the count {{ count }}</h1>
+  <button
+    class="btn w-40 bg-blue-500 text-cyan-700 py-10 rounded"
+    @click="count++"
+  >
+    Increment
+  </button>
+  <button
+    class="btn w-40 bg-red-500 text-cyan-700 py-10 rounded"
+    @click="count--"
+  >
+    Decrement
+  </button>
+</template>
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+</script>
